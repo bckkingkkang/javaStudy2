0d5cb00a347aa920fd0868506dc2d4cc4a46c0bc
kahyunKo <bufar1014@daum.net>
2021-05-05 20:52:00 +0900

    ~sec01.exam16

---
+++ src/sec01/exam15/StringIndexOfExample.java
@@ -0,0 +1,18 @@
+// 문자열 포함 여부 조사
+package sec01.exam15;
+
+public class StringIndexOfExample {
+	public static void main(String[] args) {
+		
+		String subject = "자바 프로그래밍";
+		
+		int location = subject.indexOf("프로그래밍");
+		System.out.println(location);
+		
+		if(subject.indexOf("자바") != -1) {
+			System.out.println("자바와 관련된 책입니다.");
+		} else {
+			System.out.println("자바와 관련이 없습니다.");
+		}
+	}
+}
