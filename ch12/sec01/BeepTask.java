c4d301dbd48e07562d4fe137badc2b82ed28d344
kahyunKo <bufar1014@daum.net>
2021-05-16 20:38:59 +0900

    Add files via upload

---
+++ ch12/sec01/BeepTask.java
@@ -0,0 +1,18 @@
+// 비프음을 들려주는 작업 정의
+package sec01.exam02;
+
+import java.awt.Toolkit;
+
+public class BeepTask implements Runnable {
+	public void run() {
+		
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		
+		for ( int i = 0; i < 5; i++ ) {
+			toolkit.beep();
+			try {
+				Thread.sleep(500);
+			} catch ( Exception e ) { }
+		}
+	}
+}
