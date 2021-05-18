92f075636cf1de162fb41a94740723a2124d7519
kahyunKo <bufar1014@daum.net>
2021-05-18 19:19:08 +0900

    Add files via upload

---
+++ ch12/sec01/ThreadNameExample.java
@@ -0,0 +1,19 @@
+// 메인 스레드 이름 출력 및 UserThread 생성 및 시작
+/* 메인 스레드의 참조를 얻어 스레드 이름을 콘솔에 출력하고 새로 생성한 스레드의 이름을 setName() 메소드로 설정한 후, getName() 메소드로 읽어오기 */
+package sec01.exam06;
+
+public class ThreadNameExample {
+	public static void main(String[] args) {
+		
+		/*
+		 * Thread mainThread = Thread.currentThread(); // <- 이 코드를 실행하는 스레드 객체 얻기
+		 * System.out.println("프로그램 시작 스레드 이름 : " + mainThread.getName());
+		 * 
+		 * ThreadA threadA = new ThreadA(); // ThreadA 생성
+		 * System.out.println("작업 스레드 이름 : " + threadA.getName()); threadA.start();
+		 * 
+		 * ThreadB threadB = new ThreadB(); // ThreadB 생성
+		 * System.out.println("작업 스레드 이름 : " + threadB.getName()); threadB.start();
+		 */
+	}
+}
